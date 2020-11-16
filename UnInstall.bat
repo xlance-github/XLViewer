ccb4e2e71979b4e69d67041e7a0c551cf13be7e9
xlance-github <74504527+xlance-github@users.noreply.github.com>
2020-11-16 07:45:36 -0500

    Add files via upload

---
+++ UnInstall.bat
@@ -0,0 +1,16 @@
+@echo off
+echo ____________________________________________________________________________
+echo !!!!.... Installing XLance Componant ....!!!!
+echo !
+echo !
+
+SET fwpath= "%windir%\Microsoft.NET\Framework\v2.0.50727"
+SET fwpath1= "%windir%\Microsoft.NET\Framework\v4.0.30319"
+
+%fwpath%\RegAsm.exe -u "%~dp0%\XLanceDLL.dll"
+%fwpath1%\RegAsm.exe -u "%~dp0%\XLanceDLL.dll"
+
+echo !
+echo !!!!.... Installation Finished ....!!!!
+echo ____________________________________________________________________________
+pause
